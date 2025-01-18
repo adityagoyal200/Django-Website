bae569410581554cf7dfd5d30c422f0738066dd8
Aditya goyal <goyaladitya3815@gmail.com>
2025-01-18 12:18:36 +0530

    inital files

---
+++ website/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return render(request,'index.html')
+    # return HttpResponse("Hello from home")
+
+def about(request):
+    return HttpResponse("Hello from about")
+
+def services(request):
+    return HttpResponse("Hello from services")
