2ca553b567d3f025b3b29aeda68121360368f1ae
Aditya goyal <goyaladitya3815@gmail.com>
2025-01-20 02:25:47 +0530

    learning how to send data or use django varibales in our html

--- home/views.py
+++ home/views.py
@@ -2,4 +2,9 @@
 from django.shortcuts import render,HttpResponse
 
 def web(request):
-    return render(request,"home.html")
+    data = [
+        {'name' : 'Aditya', 'age':22},
+        {"name":"Akshit","age":33}
+    ] 
+    return render(request,"home.html", context = {'data':data})
+#with the help of this context we can send out data from backend to our webpage 
