f8707c270b44b81e5a181d9dd678e00129ee4034
Aditya goyal <goyaladitya3815@gmail.com>
2025-01-24 12:59:16 +0530

    models and migration

--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+#example we have a student schema
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    email = models.EmailField()
+    parent_name = models.CharField(max_length=50)
+    address = models.TextField()
+    assignment = models.FileField()
+    
